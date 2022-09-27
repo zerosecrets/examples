64c65b0b860d263deb7372d7f4186b8461691093
Sam Magura <srmagura@gmail.com>
2022-09-27 12:17:07 -0400

    Update Python script to use Zero SDK

--- datadog-docker-compose/datadog-agent/zero-entrypoint.py
+++ datadog-docker-compose/datadog-agent/zero-entrypoint.py
@@ -9,18 +9,18 @@ if 'ZERO_TOKEN' not in os.environ:
 secrets = zero(token=ZERO_TOKEN, pick=['datadog']).fetch()
 
 if 'datadog' not in secrets:
-  print('datadog secret not found.')
-  sys.exit(1)
+    print('datadog secret not found.')
+    sys.exit(1)
 
 if 'API_KEY' not in secrets['datadog']:
-  print('API_KEY field not found.')
-  sys.exit(1)
+    print('API_KEY field not found.')
+    sys.exit(1)
 
 api_key = secrets['datadog']['API_KEY']
 
 if len(api_key) == 0:
-  print('API_KEY field is empty.')
-  sys.exit(1)
+    print('API_KEY field is empty.')
+    sys.exit(1)
 
 os.environ['DD_API_KEY'] = api_key
 os.system('/bin/entrypoint.sh')
